195ef3ad40d2bd9a1b97f391502518c889d4ed84
WerskyCode <werskycode@mail.ru>
2026-07-13 07:59:18 +0300

    add InputMismatchException before NoSuchElementException

--- src/main/kotlin/core/io/ReadearGraph.kt
+++ src/main/kotlin/core/io/ReadearGraph.kt
@@ -5,6 +5,7 @@ import java.io.File
 import core.models.graph.Graph
 import core.models.graph.Edge
 import java.io.FileNotFoundException
+import java.util.InputMismatchException
 
 // Функция для считывания графа из файла
 fun readGraphFromFile(fileName: String): Graph {
@@ -28,6 +29,14 @@ fun readGraphFromFile(fileName: String): Graph {
 
     catch (e: FileNotFoundException) {
         println("Ошибка: Файл не найден!")
+        return Graph()
+    }
+
+    // Для ошибки несоответсвия типов
+    // Это еще разновидность ошибок нехватки данных и наследник класса NoSuchElementException.
+    // Сначала надо ловить ее и потом ловить ее и потом уже NoSuchElement
+    catch (e: InputMismatchException) {
+        println("Ошибка: Несоответсвие типов! Ожидаются целые числа!")
         return Graph()
     }
 
