ba8449503ecd28080d4975d855b81797aea65094
WerskyCode <werskycode@mail.ru>
2026-07-13 04:04:57 +0300

    Added graph parser implementation and updated .gitignore for sandbox

--- src/main/kotlin/core/io/ReadearGraph.kt
+++ src/main/kotlin/core/io/ReadearGraph.kt
@@ -1,11 +1,25 @@
-package.core.io
+package core.io
 
+import core.models.graph.Edge
+import java.util.Scanner
+import java.io.File
 import core.models.graph.Graph
 
 // Пока заглушка
 fun readGraphFromFile(fileName: String): Graph {
-
     val graph = Graph()
+    val scanner = Scanner(File(fileName))
+
+    val countVertexes = scanner.nextInt()
+    val countEdges = scanner.nextInt()
+
+    repeat(countEdges) {
+        val from = scanner.nextInt()
+        val to = scanner.nextInt()
+        val weight = scanner.nextInt()
+
+        graph.edges.add(Edge(from, to, weight))
+    }
 
     return graph
 }
