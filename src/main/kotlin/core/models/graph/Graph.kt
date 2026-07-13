dd2b3a013415f3f89ac1337e8c26a4843ac7f21f
WerskyCode <werskycode@mail.ru>
2026-07-13 08:44:37 +0300

    Add Vertex class

--- src/main/kotlin/core/models/graph/Graph.kt
+++ src/main/kotlin/core/models/graph/Graph.kt
@@ -3,7 +3,7 @@ package core.models.graph
 class Graph{
     val vertexes = mutableListOf<Int>()
     val edges = mutableListOf<Edge>()
-
+    val vertices get() = vertexes
     // Проверяет повторное вхождение ребра в граф
     fun hasEdge(edge: Edge): Boolean {
         // any пройдется по всему списку
---
+++ src/main/kotlin/core/models/graph/Vertex.kt
@@ -0,0 +1,3 @@
+package core.models.graph
+
+open class Vertex(val id: Int)
